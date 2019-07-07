dd4a44088c457a11635b8d9cf26a27aefcaa75ec
Marc Poppy <poppy.marc@gmail.com>
2019-07-07 21:51:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,6 +19,9 @@ class Scraper
 
     def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+      doc.css(".post").each do |post|
+        
+      end
       binding.pry
     end
 
